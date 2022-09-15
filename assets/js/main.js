358e88761fcfd979741122ecbe30080b03ffa52b
KostasMateer <komateer@davidson.edu>
2022-09-14 22:50:54 -0400

    final submission

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-
 /* javascript */
 
 var bauhaus = `
@@ -23,6 +22,9 @@ let output = document.getElementById("output");
 window.addEventListener("mousemove", (e) => {
   let xPos = e.clientX;
   let yPos = e.clientY;
-  var coordinates = "X: " + xPos + " " + "Y: " + yPos
-  output.innerHTML = coordinates
+  var coordinates = "X: " + xPos + " " + "Y: " + yPos;
+  output.innerHTML = coordinates;
 });
+
+var elementList = document.querySelectorAll("div");
+console.log(`${elementList.length} div elements making self portrait`);
